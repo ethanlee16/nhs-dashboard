cf59501421456dcfabedee996bb4532e6c194467
Ethan Lee <ethanlee@ethansites.com>
2016-09-16 12:44:15 -0700

    Added production Firebase info

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
+var IS_DEV = app.get('env') === 'development';
+
+if (!IS_DEV) {
+  var fs = require('fs');
+  fs.writeFileSync('private.json', process.env.FIREBASE);
+}
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -23,8 +29,8 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
-    message: err.message,
-    error: app.get('env') === 'development' ? err : {}
+  message: err.message,
+  error: IS_DEV ? err : {}
   });
 });
 
