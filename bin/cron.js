4371cbd289cbe01816e9751cfb5dc558846363d0
Ethan Lee <ethanlee@ethansites.com>
2016-10-17 07:57:28 -0700

    fix cron job

--- bin/cron.js
+++ bin/cron.js
@@ -1,7 +1,12 @@
 var firebase = require('firebase');
 var twilio = require('twilio');
 var path = require('path');
-var request = require('request');
+var request = require('request-promise');
+
+if (process.env.NODE_ENV !== 'development') {
+  var fs = require('fs');
+  fs.writeFileSync('private.json', process.env.FIREBASE);
+}
 
 firebase.initializeApp({
   databaseURL: "https://nhs-signup.firebaseio.com/",
@@ -13,25 +18,26 @@ var users = db.ref('users');
 var info = db.ref('info');
 var client = new twilio.RestClient(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
-var refreshToken = process.env.OUTLOOK_TOKEN;
-
-request('https://login.microsoftonline.com/common/oauth2/v2.0/token', {
+request(`https://login.microsoftonline.com
+  /common/oauth2/v2.0/token`, {
   method: 'POST',
   form: {
     'grant_type': 'refresh_token',
     'client_id': process.env.OUTLOOK_CLIENT_ID,
     'client_secret': process.env.OUTLOOK_CLIENT_SECRET,
-    'refresh_token': refreshToken
+    'refresh_token': process.env.OUTLOOK_TOKEN
   },
   headers: {
     'Content-Type': 'x-www-form-urlencoded'
   },
   json: true
 }).then(response => {
-  info.child('outlook').set({
+  return info.child('outlook').set({
     accessToken: response.access_token
   });
+}).then(snapshot => {
+  process.exit(0);
 }).catch(err => {
   console.error(err);
   process.exit(1);
-})
+});
